800487a51ce6a39e65dd29d1033f7e72eca31e60
Ondrej Cifka <cifkao@users.noreply.github.com>
2020-02-16 22:27:26 +0100

    Update package configuration

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 import setuptools
 import sys
 
+version = {}
+with open("confugue/version.py") as f:
+    exec(f.read(), version)
 
 setuptools.setup(
     name="confugue",
+    version=version.__version__,
     author="Ondřej Cífka",
-    description="A configuration framework",
+    description="Hierarchical configuration framework",
     packages=setuptools.find_packages(),
-    python_requires='>=3.6',
+    python_requires='>=3.3',
     install_requires=[
         'cached_property',
         'pyyaml',
